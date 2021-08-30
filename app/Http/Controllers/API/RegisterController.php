850b81921a15579e330c21a8d1edba55d054436e
himanshu-sharma <himanshu@radtech.pro>
2021-08-30 18:42:48 +0530

    make swagger api for login and sign up

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -17,19 +17,20 @@ class RegisterController extends BaseController
      */
     /**
  * @OA\Post(
- * path="/login",
- * summary="Sign in",
- * description="Login by email, password",
- * operationId="authLogin",
+ * path="/api/register",
+ * summary="Sign up",
+ * description="Sign up by name, email, password",
+ * operationId="authSign",
  * tags={"auth"},
  * @OA\RequestBody(
  *    required=true,
  *    description="Pass user credentials",
  *    @OA\JsonContent(
- *       required={"email","password"},
+ *       required={"user_name", "email","password", "confirm_password"},
+ *       @OA\Property(property="user_name", type="string", format="text", example="david"),
  *       @OA\Property(property="email", type="string", format="email", example="user1@mail.com"),
  *       @OA\Property(property="password", type="string", format="password", example="PassWord12345"),
- *       @OA\Property(property="persistent", type="boolean", example="true"),
+ *       @OA\Property(property="confirm_password", type="string", format="password", example="PassWord12345"),
  *    ),
  * ),
  * @OA\Response(
@@ -44,16 +45,16 @@ class RegisterController extends BaseController
     public function register(Request $request)
     {
         $this->validate($request, [
-            'name' => 'required',
+            'user_name' => 'required',
             'email' => 'required|email',
             'password' => 'required',
-            'c_password' => 'required|same:password',
+            'confirm_password' => 'required|same:password',
         ]);
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
         $success['token'] =  $user->createToken('MyApp')->plainTextToken;
-        $success['name'] =  $user->name;
+        $success['user_name'] =  $user->user_name;
         return $this->sendResponse($success, 'User register successfully.');
     }
    
@@ -62,11 +63,37 @@ class RegisterController extends BaseController
      *
      * @return \Illuminate\Http\Response
      */
+
+        /**
+ * @OA\Post(
+ * path="/api/login",
+ * summary="Sign in",
+ * description="Login by email, password",
+ * operationId="authLogin",
+ * tags={"auth"},
+ * @OA\RequestBody(
+ *    required=true,
+ *    description="Pass user credentials",
+ *    @OA\JsonContent(
+ *       required={"email","password"},
+ *       @OA\Property(property="email", type="string", format="email", example="user1@mail.com"),
+ *       @OA\Property(property="password", type="string", format="password", example="PassWord12345"),
+ *    ),
+ * ),
+ * @OA\Response(
+ *    response=422,
+ *    description="Wrong credentials response",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="message", type="string", example="Sorry, wrong email address or password. Please try again")
+ *        )
+ *     )
+ * )
+ */
     public function login(Request $request){
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
             $user = Auth::user(); 
             $success['token'] =  $user->createToken('MyApp')->plainTextToken; 
-            $success['name'] =  $user->name;
+            $success['user_name'] =  $user->user_name;
             return $this->sendResponse($success, 'User login successfully.');
         }else{ 
             return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
